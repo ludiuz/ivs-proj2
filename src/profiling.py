7aa46a76aa9f9fd5e8a298f50a638bb484053a25
peterg127 <peter.gvozdjak1@gmail.com>
2023-04-25 23:58:10 +0200

    Profiling done
    It should calculate correctly sample standard deviation

--- src/profiling.py
+++ src/profiling.py
@@ -3,17 +3,22 @@ import calculator as c
 
 zoz = []
 calc = c.Calculator()
-with open("data.txt", "r") as f:
+with open('data.txt' , 'r') as f:
+    #zoz = zoz + f.read().split()
     for line in f.readlines():
         zoz = zoz + line.split()
 print(zoz)
 sum = ""
 res_i = ""
 for i in range(len(zoz)):
-    sum = calc(str(sum) + "+" + zoz[i])
-    res_i = calc(str(res_i) + "+" + zoz[i] + "^2")
-    # print(res_i)
-avg = calc(sum + "/" + str(len(zoz)))
-
-# avg =
-# average = sum(zoz)
+    sum = calc(str(sum) + '+' + zoz[i])
+    res_i = calc(str(res_i) + '+' + zoz[i] + '^2')
+    #print(res_i)
+avg = calc(sum + '/' + str(len(zoz)))
+first = calc((str(res_i) +'-'+ str(len(zoz))+'*('+ avg + ')^2'))
+print(first)
+res_2 = calc('(1/(' + str(len(zoz)) + '-1))*' + first)
+res = calc('('+ str(res_2) + ')^(1/2)')
+print(res)
+#avg = 
+#average = sum(zoz)
